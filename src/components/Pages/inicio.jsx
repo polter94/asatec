f2276fbaece2c13ae20e54078ef7c47c2cedaacd
Marco <marcodutto1994@gmail.com>
2024-12-04 13:53:41 -0300

    se avanza pagina de inicio

--- src/components/Pages/inicio.jsx
+++ src/components/Pages/inicio.jsx
@@ -6,6 +6,14 @@ const inicio = () => {
     <div>
 
       <div className="inicio-background">
+        <div className="container">
+          <h2>BIENVENIDO</h2>
+          <p className="text-box">Únete a nuestra comunidad y accede a una amplia red de contactos. Aumenta tus conocimientos, mejora tus habilidades y expande tus oportunidades laborales.</p>
+          <p>¡En AES+ siempre hay algo nuevo que aprender!</p>
+        </div>
+      </div>
+      <hr />
+      <div className="inicio-background">
         <div>inicio</div>
       </div>
 
